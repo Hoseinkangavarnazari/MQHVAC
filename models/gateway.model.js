959737c1a4c0ed62a4c24c643629b48b57d5f6c0
hoseinkangavarnazari <hkangavarnazari@gmail.com>
2020-08-17 12:48:25 +0430

    Adding the gateway DB

---
+++ models/gateway.model.js
@@ -0,0 +1,61 @@
+var mongoose = require('mongoose');
+var Schema = mongoose.Schema;
+
+const connection = require("./conn_db");
+
+var gateway = new Schema({
+
+    gid: {
+        type: number,
+        required: true,
+        unique: true
+    },
+    location: {
+        type: String,
+        default: ""
+    },
+    relay: [{
+        rid: Number,
+        detail: String,
+        location: {
+            type: String,
+            default: ""
+        },
+        upThreshold: {
+            type: Number,
+            default: 30
+        },
+        downThreshold: {
+            type: Number,
+            default: 15
+        }
+    }],
+    control: {
+        type: String,
+        required: true,
+        enum: ['m', 'ts', 't', 't'],
+        default: 't'
+    },
+    scheduleFlag: {
+        type: Boolean,
+        default: false
+    },
+    termostatFlag: {
+        type: Boolean,
+        default: false
+    },
+    sensor: [{
+        sid: Number,
+        location: {
+            type: String,
+            default: ""
+        }
+    }],
+    mapURL: String
+
+});
+
+//  { -> :  referes to }in control m -> manual, ts-> termostat/schedule, t-> termostat, s->schedule
+
+
+module.exports = mongoose.model('emergencyCall', emergencyCallSchema);
