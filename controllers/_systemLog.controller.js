44cc54a76b1121613ad4fb72cb7ee44e36432cf2
hoseinkangavarnazari <hkangavarnazari@gmail.com>
2020-09-16 17:22:25 +0430

    LOG API is added

--- controllers/_systemLog.controller.js
+++ controllers/_systemLog.controller.js
@@ -158,8 +158,33 @@ exports.reteriveUnseen = async (req, res) => {
  * (1) returns all logs related for requested aid
  */
 exports.log = async (req, res) => {
-    console.log("You hit the endpoint");
-    res.status(200).send("Temp response");
+    let aid = req.body.aid;
+    actuator = await Actuator.find({
+        aid: aid
+    });
+    if (actuator.length == 0) {
+        res.status(404).send("Requested actuator doesn't exists in db.");
+        return;
+    } else {
+
+        var logStorage = {};
+        logStorage[aid] = []
+
+        doc = await systemLog.find({
+            aid: aid
+        });
+        if (doc.length >= 0) {
+            for (var i = 0; i < doc.length; i++) {
+                    logStorage[aid].push(doc[i])
+            }
+
+            res.status(200).send(logStorage);
+        } else {
+            res.status(404).send({
+                msg: "No log found."
+            });
+        }
+    }
 }
 
 
